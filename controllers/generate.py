26b80a67f9c92e7fc6953349846437056aa7c60c
Hilson Shrestha <hilsonshrestha@gmail.com>
2015-08-03 21:52:38 +0545

    cleanup

--- controllers/generate.py
+++ controllers/generate.py
@@ -9,9 +9,7 @@ import json
 import copy
 
 def findTiming(day, duration):
-	# [1,2,4,5,6] 3
-	
-
+	# [1,2,4,5,6]    3
 	if not day:
 		return []
 	r = []
@@ -23,70 +21,17 @@ def findTiming(day, duration):
 				# days are continuous 
 				d -= 1
 				if d == 0:
-					# r['timing'].append([day[i], day[j]])
 					r.append([day[i], day[j]])
 			k += 1
 	return r
 
-	# if not day:
-	# 	return []
-	# r = {"duration": duration,
-	# 	"timing": []
-	# }
-	# r = []
-	# for i in range(0, len(day)):
-	# 	d = duration
-	# 	k = 0
-	# 	for j in range(i, len(day)):
-	# 		if i + k == j:
-	# 			# days are continuous 
-	# 			d -= 1
-	# 			if d == 0:
-	# 				# r['timing'].append([day[i], day[j]])
-	# 				r.append([day[i], day[j]])
-	# 		k += 1
-	# return r
-
 def findTeachingTime(subject, available_teaching_time, total_classes, class_duration, routine, possibilities):
-	# print subject, available_teaching_time, total_classes, class_duration, routine
-	# viable_days = []
 	for day in routine:
-		# day_has_subject = False
-		# for r in routine[day]:
-		# 	if r[0] == subject:
-		# 		day_has_subject = True
-		# 		break;
-
-		# if day_has_subject == False:
-			# no subject in that day..
-			# so find if teacher has available time in that class.
-		
-
 		if day in available_teaching_time:
 			pt = findTiming(available_teaching_time[day], class_duration)
-			
 			if len(pt):
-				# if day not in viable_days:
-					# viable_days.append(day)
 				possibilities[day][class_duration] = pt
-		# randomday = viable_days[random.randint(0, len(viable_days) - 1)]
-		# print randomday
-	# print viable_days
-			# print day, findTiming(available_teaching_time[day], class_duration)
-	
-
-
-
-
 	return False
-
-
-
-
-
-
-
-
 
 def overlaps(a, b):
 	if a[1] < b[0] or a[0] > b[1]:
@@ -98,51 +43,17 @@ def get_random_day_for_duration(possibilities, duration, routine):
 	# remove overlapping classes from possibilities
 
 	for day in routine:
-
 		for class_ in routine[day]:
-			# if day in possibilities
-				for class_duration in possibilities[day]:
-					toremove = []
-					for x in possibilities[day][class_duration]:
-						if overlaps(x, class_['timing']):
-							print x, class_['timing']
-							toremove.append(x)
-					for x in toremove:
-						possibilities[day][class_duration].remove(x)
-			# print day, class_
-
-	# for k_day in possibilities:
-	# 	x = possibilities[k_day] # "1": [[1, 1 ], [3, 3 ], [4, 4 ] ], "2" :[..]
-	# 	for y in x:
-	# 			z = x[y] #[[1, 1 ], [3, 3 ], [4, 4 ] ]
-	# 		# for day in routine:
-	# 			# for class_ in routine[day]:
-	# 			print k_day, z, routine[k_day]
-	# 			# for class_ in routine[k_day]:
-	# 			# 	v = class_['timing']
-	# 			# 	for u in z:
-	# 			# 		if not (u[1] < v[0] or u[0] > v[1]): 
-	# 			# 			# z.remove(class_['timing'])
-	# 			# 			# print u, v, k_day
-	# 			# 			z.remove(u)
-
-	# 						# pass
-
-	# 				# if class_['timing'] in z:
-	# 					# z.remove(class_['timing'])
-
+			for class_duration in possibilities[day]:
+				toremove = []
+				for x in possibilities[day][class_duration]:
+					if overlaps(x, class_['timing']):
+						print x, class_['timing']
+						toremove.append(x)
+				for x in toremove:
+					possibilities[day][class_duration].remove(x)
 	keys = [key for key in possibilities if possibilities[key] != {} and duration in possibilities[key]]
-
-  # print keys
-	# print possibilities
-	# return "sunday"
 	r = keys[random.randint(0, len(keys) - 1)]
-	# now delete from possibilities of class and from teacher's avaliable timing
-	# rtime = 
-	# print possibilities[r]
-	# print "__"
-	# print possibilities[r][duration]
-
 	return r, random.choice(possibilities[r][duration])
 
 class GenerateAjaxHandler(BaseHandler):
@@ -213,14 +124,10 @@ class GenerateAjaxHandler(BaseHandler):
 							possibilities = class_distribution[subject]['possibilities']
 						)
 
-
-
-
-
 		total_tries = 0
 		original_classes = copy.deepcopy(classes)
 		inner_fail = True
-		while total_tries < 5 and inner_fail == True:
+		while total_tries < 50 and inner_fail == True:
 			inner_fail = False
 			total_tries += 1
 			classes = copy.deepcopy(original_classes)
@@ -243,11 +150,6 @@ class GenerateAjaxHandler(BaseHandler):
 							except:
 								tries += 1
 								continue	
-							# print class_distribution[subject]['possibilities']
-							# print randomday, i, subject, randomtime
-							# delete class_distribution[subject]['possibilities'][randomday]
-							# class_distribution[subject]['possibilities'].pop(randomday, None)
-							# print class_distribution[subject]['possibilities']
 							t = Teacher.get_by_id(class_distribution[subject]['teacher'])
 							routine[randomday].append({
 								'subject': subject,
@@ -261,24 +163,12 @@ class GenerateAjaxHandler(BaseHandler):
 							# print "--"
 							print routine
 
-
-
-
-							# for day in routine:
-							# 	for class_ in routine[day]:
-							# 		print day, class_
-
-
 							distribution[str(i)] -= 1 #reduce total classes
-							# print distribution[str[i]]
-
 		self.response.headers['Content-Type'] = 'application/json'
 		# self.write(json.dumps(original_classes,sort_keys=True, indent=4))
-
 		self.write(json.dumps(classes,sort_keys=True, indent=4))
-
+		# self.write(classes)
 
 class GenerateHandler(BaseHandler):
 	def get(self):
-		# departments = db.GqlQuery("SELECT * from Department ORDER BY department_name")
 		self.render("generate.html")
